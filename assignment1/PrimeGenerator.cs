5ce0073780de9e7994328dc0b360bb4017e05ec2
Mohammed Mahmoud <loosmm@gmail.com>
2020-10-02 04:11:35 +0200

    Add files via upload

---
+++ assignment1/PrimeGenerator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    class PrimeGenerator
+    {
+        private static object _lockObj = new object();
+        public static List<long> GetPrimesSequential(long first, long last)
+        {
+
+            var primes = new List<long>();
+            int flag, j;
+
+            for (long i = first; i < last; i++)
+            {
+                
+                    flag = 1;
+
+                    for (j = 2; j <= i / 2; ++j)
+                    {
+                        if (i % j == 0)
+                        {
+                            flag = 0;
+                            break;
+                        }
+                    }
+
+                if (flag == 1)
+                {                 
+                    primes.Add(i);               
+                }               
+
+            }
+            return primes;
+        }
+
+
+        public static List<long> GetPrimesParallel(long first, long last)
+        {
+
+             List<long> primes = new List<long>();
+             int flag, j;
+
+            Parallel.For(first, last, i =>
+            {
+
+                flag = 1;
+
+                for (j = 2; j <= i / 2; ++j)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+
+                if (flag == 1) {
+
+                    lock (_lockObj)
+                    {
+                        primes.Add(i);
+                    }
+                   
+
+                } 
+                
+            });
+
+            return primes.AsParallel().AsOrdered().ToList();
+            
+        }
+        
+    }
+
+  
+
+}
+
---
+++ assignment1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Diagnostics;
+
+namespace assignment1
+{
+    class Program
+    {
+
+        private static void MessureTime(Action p)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            p.Invoke();
+            sw.Stop();
+            Console.WriteLine("Time = {0:F5} sec.", sw.ElapsedMilliseconds / 1000d);
+        }
+        static void Main(string[] args)
+        {
+            // implement the function using a sequential approach, and check for correctness.
+
+            List<long> t = PrimeGenerator.GetPrimesSequential(1,100);
+
+            foreach(long items in t)
+               Console.WriteLine(items);
+
+
+            // implement the function using parallel programming, and check for correctness
+            List<long> t1 = PrimeGenerator.GetPrimesParallel(1, 100);
+
+            foreach (long items in t1)
+            {
+                Console.WriteLine(items);
+
+            }
+
+            Console.WriteLine("Checking performance for parallel approach between 1, 1000000");
+            MessureTime(() => PrimeGenerator.GetPrimesParallel(1, 1000000));
+           
+            Console.WriteLine("Checking performance for sequential approach between 1, 1000000");
+            MessureTime(() => PrimeGenerator.GetPrimesSequential(1, 1000000));
+            
+            Console.WriteLine("Checking performance for parallel approach between 1, 10000000");
+            MessureTime(() => PrimeGenerator.GetPrimesParallel(1, 10000000));
+            
+            Console.WriteLine("Checking performance for sequential approach between 1, 10000000");
+            MessureTime(() => PrimeGenerator.GetPrimesSequential(1, 10000000));
+            
+            Console.WriteLine("Checking performance for parallel approach between 1000000, 2000000");
+            MessureTime(() => PrimeGenerator.GetPrimesParallel(1000000, 2000000));
+            
+            Console.WriteLine("Checking performance for sequential approach between 1000000, 2000000");
+            MessureTime(() => PrimeGenerator.GetPrimesSequential(1000000, 2000000));
+            
+            Console.WriteLine("Checking performance for parallel approach between 10000000, 20000000");
+            MessureTime(() => PrimeGenerator.GetPrimesParallel(10000000, 20000000));
+            
+            Console.WriteLine("Checking performance for sequential approach between 10000000, 20000000");
+            MessureTime(() => PrimeGenerator.GetPrimesSequential(10000000, 20000000));
+
+
+        }
+    }
+}
